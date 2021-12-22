d5226b394b8eb5ad6428d99a98aa9776fb81b269
0xhatsume <0xhatsume@gmail.com>
2021-12-22 23:49:05 +0800

    feat: add boiler data for force graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ForceGraph2D from 'react-force-graph-2d';
 
 function App() {
+
+
+  const data = {
+    nodes: [{id: "1"},{id: "2"},{id: "3"},{id: "4"}],
+    links: [{ "source": "1", "target": "2" },{ "source": "1", "target": "3" },{ "source": "3", "target": "4" },]
+  }
   return (
     <div className="App">
-      Hi Frenssss~~~~
+      Simple Force Graph First
+      <ForceGraph2D
+        graphData={data}
+      />
     </div>
   );
 }
