0fa5ddbeb77d54460dc01df8048f7c91e8d571ce
0xhatsume <0xhatsume@gmail.com>
2021-12-23 19:32:14 +0800

    feat: trial data for react-force-graph

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ function App() {
 
 
   const data = {
-    nodes: [{id: "1"},{id: "2"},{id: "3"},{id: "4"}],
-    links: [{ "source": "1", "target": "2" },{ "source": "1", "target": "3" },{ "source": "3", "target": "4" },]
+    nodes: [{id: "1"},{id: "2"},{id: "3"},{id: "4"}, {id:"5"},{id:"6"},{id:"7"}],
+    links: [{ "source": "1", "target": "2" },
+            { "source": "1", "target": "3" },
+            { "source": "3", "target": "4" },   
+            {"source": "5", "target": "6"},
+            {"source": "5", "target": "7"},
+          
+          ]
   }
   return (
     <div className="App">
-      Simple Force Graph First
+      Simple Force Graph Firsts ...
       <ForceGraph2D
         graphData={data}
+        dagMode={'lr'}
+        nodeId="id"
       />
     </div>
   );
